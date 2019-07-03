44f4383db933c1aa963b80605622d5e0cca505a3
Demetri Mihalakakos <demetri@Demetris-MacBook-Pro.local>
2019-07-03 16:03:14 -0400

    Added new services

--- app/boot/services.php
+++ app/boot/services.php
@@ -10,6 +10,11 @@ $app->addService('errorHandler', function() {
 	return new wiggum\services\errorHandler\ErrorHandler($this);
 });
 
+$app->addService('dictionary', function() {
+    $dictionary = $this->config->get('dictionary', []);
+    return new wiggum\services\dictionary\Dictionary($dictionary);
+});
+    
 $app->addService('template', function() {
 	return new wiggum\services\template\TemplateFactory($this);
 });
